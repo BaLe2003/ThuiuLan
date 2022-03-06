f10a9b6d7755f719adb31eeb181585f9b6eeaf6f
Le Quang Ba <77970250+BaLe2003@users.noreply.github.com>
2022-03-06 11:42:59 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,17 @@
+const CONFIG = {
+    titleWeb: "ThuiuLam",
+    introTitle: 'Heluu bạn iu!',
+    introDesc: `Mình có điều này muốn hỏi bạn nhớ phải trả lời thật lòng nhaaa:3333`,
+    btnIntro: 'Dạ!',
+    title: 'bạn iu mình có phải không nào ._.',
+    desc: 'Nếu bạn ko trả lời mà thoát ra tức là muốn làm vợ mình rùi đó nha :v ',
+    btnYes: 'Yêu ơi là yêu <3',
+    btnNo: 'Bạn mơ à ???',
+    question: 'Lí do bạn thích mình đi :vvv',
+    btnReply: 'Gửi cho mình <3',
+    reply: 'Vì bạn đẹp try và dethuong vlll lun asaaa ♥, mình iu bạn lắm lắmmmm',
+    mess: 'Mình biết mà ^^ Yêu bạn x1000.000',
+    messDesc: 'Tối nay mình qua đón bạn đi chơi nhaa :v Còn giờ thì chờ gì nữa mà ko inbox cho mình đi nàooo',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://facebook.com/changtraithe.ki' 
+}
